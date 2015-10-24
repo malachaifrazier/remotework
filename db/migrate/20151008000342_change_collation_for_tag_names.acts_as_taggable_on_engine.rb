daaff8deb10a812a2f725459642c5d16a2dac4d5
Mike Desjardins <desjardinsmike@gmail.com>
2015-10-24 18:57:08 -0400

    Removing acts_as_taggable_on was more annoying than I thought it'd be. :/

--- db/migrate/20151008000342_change_collation_for_tag_names.acts_as_taggable_on_engine.rb
+++ db/migrate/20151008000342_change_collation_for_tag_names.acts_as_taggable_on_engine.rb
@@ -3,8 +3,8 @@
 # work properly
 class ChangeCollationForTagNames < ActiveRecord::Migration
   def up
-    if ActsAsTaggableOn::Utils.using_mysql?
-      execute("ALTER TABLE tags MODIFY name varchar(255) CHARACTER SET utf8 COLLATE utf8_bin;")
-    end
+#    if ActsAsTaggableOn::Utils.using_mysql?
+#      execute("ALTER TABLE tags MODIFY name varchar(255) CHARACTER SET utf8 COLLATE utf8_bin;")
+#    end
   end
 end
