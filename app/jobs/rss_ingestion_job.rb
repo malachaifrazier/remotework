7efe1d004fcb3de03021ca0593172943047cc946
Mike Desjardins <desjardinsmike@gmail.com>
2015-10-26 08:47:24 -0400

    Make sure sucker punch doesn't exhaust AR connections.

--- app/jobs/rss_ingestion_job.rb
+++ app/jobs/rss_ingestion_job.rb
@@ -4,21 +4,23 @@ class RssIngestionJob < ActiveJob::Base
   queue_as :low
 
   def perform(klasses, opts={})
-    Job.delete_all if opts[:purge_all]
-    klasses = [klasses] unless klasses.is_a?(Array)
+    ActiveRecord::Base.connection_pool.with_connection do
+      Job.delete_all if opts[:purge_all]
+      klasses = [klasses] unless klasses.is_a?(Array)
 
-    klasses.each do |klass|
-      Rails.logger.info "Processing jobs from #{klass}"
-      klazz = klass.constantize
-      klazz.feed_urls.each do |feed_url|
-        Rails.logger.info "Reading URL #{feed_url}"
-        klazz.delete_all if opts[:purge_source]
-        feed = Feedjira::Feed.fetch_and_parse feed_url
-        feed.entries.each do |entry|
-          job = klazz.factory(entry, feed)
-          if job
-            job.description = fetch_description(job.original_post_url) unless klazz.skip_description_scrape?
-            job.save! unless Job.probable_duplicate(job).exists?
+      klasses.each do |klass|
+        Rails.logger.info "Processing jobs from #{klass}"
+        klazz = klass.constantize
+        klazz.feed_urls.each do |feed_url|
+          Rails.logger.info "Reading URL #{feed_url}"
+          klazz.delete_all if opts[:purge_source]
+          feed = Feedjira::Feed.fetch_and_parse feed_url
+          feed.entries.each do |entry|
+            job = klazz.factory(entry, feed)
+            if job
+              job.description = fetch_description(job.original_post_url) unless klazz.skip_description_scrape?
+              job.save! unless Job.probable_duplicate(job).exists?
+            end
           end
         end
       end
--- app/jobs/tweet_job.rb
+++ app/jobs/tweet_job.rb
@@ -2,8 +2,10 @@ class TweetJob < ActiveJob::Base
   queue_as :low
 
   def perform
-    job = Job.next_up_for_tweet.limit(1).first
-    TweetService.new(job).tweet
-    job.touch(:last_tweeted_at)
+    ActiveRecord::Base.connection_pool.with_connection do
+      job = Job.next_up_for_tweet.limit(1).first
+      TweetService.new(job).tweet
+      job.touch(:last_tweeted_at)
+    end
   end
 end
