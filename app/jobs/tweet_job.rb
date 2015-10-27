cb66bb2ada4afa43ce1b4a27466c4dd588af5917
Mike Desjardins <desjardinsmike@gmail.com>
2015-10-27 16:15:41 -0400

    Move last_tweeted_at update to before actual tweet?

--- app/jobs/tweet_job.rb
+++ app/jobs/tweet_job.rb
@@ -4,10 +4,10 @@ class TweetJob < ActiveJob::Base
   def perform
     ActiveRecord::Base.connection_pool.with_connection do
       job = Job.next_up_for_tweet.limit(1).first
-      TweetService.new(job).tweet
       #  job.touch(:last_tweeted_at)  WTF, not working???
       job.last_tweeted_at = Time.zone.now
       job.save!
+      TweetService.new(job).tweet
     end
   end
 end
