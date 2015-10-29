a9f2f969e3dfd951c11ff27ccb10f1b90cb9be88
Mike Desjardins <desjardinsmike@gmail.com>
2015-10-28 21:30:41 -0400

    Trying to add another server to the mix.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,7 @@
 # Change these
-server '45.55.248.140', port: 22, roles: [:web, :app, :db], primary: true
+server '45.55.248.140', port: 22, roles: [:web, :app], primary: true
+server '159.203.77.129', port: 22, roles: [:web, :app]
+
 
 set :repo_url,        'git@github.com:mdesjardins/remotework.git'
 set :application,     'remotework'
