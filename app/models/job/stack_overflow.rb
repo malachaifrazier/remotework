161b2d3a6cf694bc3171dea6d899293e0b16a3e0
Mike Desjardins <desjardinsmike@gmail.com>
2015-10-30 11:00:19 -0400

    Stop scraping SO job descriptions to avoid 999 Request Denied. :(

--- app/models/job/stack_overflow.rb
+++ app/models/job/stack_overflow.rb
@@ -23,4 +23,8 @@ class Job::StackOverflow < Job
   def rebuild_tags!(category, other)
     self.tags = TagBuilder.new(category, self.title, self.description, other).tags[:all]
   end
+
+  def skip_description_scrape?
+    true
+  end
 end
