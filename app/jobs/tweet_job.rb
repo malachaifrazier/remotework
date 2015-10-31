4d230f1b0878bfdb16c9693680f66a7044cf230e
Mike Desjardins <desjardinsmike@gmail.com>
2015-10-31 12:45:48 -0400

    Try again to fix the tweet timestamp issue.

--- app/jobs/tweet_job.rb
+++ app/jobs/tweet_job.rb
@@ -4,9 +4,9 @@ class TweetJob < ActiveJob::Base
   def perform
     ActiveRecord::Base.connection_pool.with_connection do
       job = Job.next_up_for_tweet.limit(1).first
-      #  job.touch(:last_tweeted_at)  WTF, not working???
-      job.last_tweeted_at = Time.zone.now
-      job.save!
+      # job.touch(:last_tweeted_at)  WTF, not working???
+      # job.save!
+      job.update_column(:last_tweeted_at, Time.zone.now)
       TweetService.new(job).tweet
     end
   end
