5ac3a429d168cd4a4365c37deccdb729cc53cac8
Mike Desjardins <desjardinsmike@gmail.com>
2015-11-01 21:18:05 -0500

    Remove connection pool stuff from Tweet job.

--- app/jobs/tweet_job.rb
+++ app/jobs/tweet_job.rb
@@ -2,12 +2,10 @@ class TweetJob < ActiveJob::Base
   queue_as :low
 
   def perform
-    ActiveRecord::Base.connection_pool.with_connection do
-      job = Job.next_up_for_tweet.limit(1).first
-      # job.touch(:last_tweeted_at)  WTF, not working???
-      # job.save!
-      job.update_column(:last_tweeted_at, Time.zone.now)
-      TweetService.new(job).tweet
-    end
+    job = Job.next_up_for_tweet.limit(1).first
+    # job.touch(:last_tweeted_at)  WTF, not working???
+    # job.save!
+    job.update_column(:last_tweeted_at, Time.zone.now)
+    TweetService.new(job).tweet
   end
 end
